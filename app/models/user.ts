62aae8cd0b1dcb3d81ec56b128a1ad11579f8ba7
greimer29 <greimerabelguzman29@gmail.com>
2024-02-27 08:45:55 -0400

    aja

---
+++ app/models/user.ts
@@ -0,0 +1,36 @@
+import { DateTime } from 'luxon'
+import { withAuthFinder } from '@adonisjs/auth'
+import hash from '@adonisjs/core/services/hash'
+import { compose } from '@adonisjs/core/helpers'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
+  uids: ['email'],
+  passwordColumnName: 'password',
+})
+
+export default class User extends compose(BaseModel, AuthFinder) {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare nombre: string
+
+  @column()
+  declare apellido: string
+
+  @column()
+  declare cargo: string
+
+  @column()
+  declare hora_entrada: string
+
+  @column()
+  declare hora_salida: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime | null
+}
---
+++ database/migrations/1708634328750_create_users_table.ts
@@ -0,0 +1,23 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').notNullable()
+      table.string('nombre')
+      table.string('apellido')
+      table.string('cargo')
+      table.string('hora_entrada')
+      table.string('hora_salida')
+
+      table.timestamp('created_at').notNullable()
+      table.timestamp('updated_at').nullable()
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
